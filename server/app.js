b0930fef8db8dc580d683669ec9064dee653d796
Артемьев Игорь Борисович <IBArtemyev@alfabank.ru>
2018-05-22 15:36:55 +0300

    fix crush app if uri is incorrect

--- server/app.js
+++ server/app.js
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 
 //uri подключения к удаленной монге
 //на localhost выглядеть будет так: mongodb://localhost:27017/exampleDb
-//TODO вынести подключение к БД в отдельную функцию, вызывать с параметром uri, т.е. добавить выбор
 // MongoClient.connect('mongodb://172.28.66.53:27017/ufr_cardfix', function (err, client) {
 //     if (err) {
 //         console.log(err)
@@ -69,7 +68,7 @@ app.post('/senddata', function(req, res) {
     console.log('here');
     console.log('\x1b[47m%s\x1b[0m',req.body);
     db.collection('cash').insert(req.body,function(err, result) {
-        if (err) throw err;
+        if (err) {console.log(err)}
         else
         console.log('\x1b[34m%s\x1b[0m','1 document inserted');
         res.send('ok');//connection.close();
@@ -80,7 +79,7 @@ app.post('/senddata', function(req, res) {
 app.post('/getdata', function(req,res) {
 
     db.collection('cash').find(req.body).toArray(function(err, result) {
-        if (err) throw err;
+        if (err) {console.log(err)}
         else
             console.log('receive data');
             console.log('\x1b[45m%s\x1b[0m',JSON.stringify(result));
@@ -92,7 +91,7 @@ app.post('/getdata', function(req,res) {
 app.post('/getlastdata', function(req,res) {
 
     db.collection('cash').find(req.body).sort({ $natural: -1 }).limit(1).toArray(function(err, result) {
-        if (err) throw err;
+        if (err) {console.log(err)}
         else
             console.log('\x1b[33m%s\x1b[0m','receive last data');
         console.log('\x1b[45m%s\x1b[0m',JSON.stringify(result));
@@ -104,15 +103,16 @@ app.post('/getlastdata', function(req,res) {
 app.get('/getschedule', function(req,res) {
     console.log(req.query);
     db.collection('cash').find(req.query).toArray(function(err, result) {
-        if (err) throw err;
+        if (err) {console.log(err)}
         else
-            console.log('\x1b[36m%s\x1b[0m','this is stuff you want');
+        {
+            console.log('\x1b[36m%s\x1b[0m', 'this is stuff you want');
             console.log(req.query.mnemonic);
             console.log(result.length);
-            
+
             //console.log(req.query.mnemonic.length);
-             result.length == 0 ? res.send('{'+req.query.mnemonic+':'+'{}'+'}') : req.query.mnemonic == undefined ? res.send(result[0]['MO2B']) : res.send(JSON.parse('{'+'"'+req.query.mnemonic+'"'+':'+JSON.stringify(result[0][req.query.mnemonic])+'}'));
-    });
+            result.length == 0 ? res.send('{' + req.query.mnemonic + ':' + '{}' + '}') : req.query.mnemonic == undefined ? res.send(result[0]['MO2B']) : res.send(JSON.parse('{' + '"' + req.query.mnemonic + '"' + ':' + JSON.stringify(result[0][req.query.mnemonic]) + '}'));
+        }});
 });
 
 
@@ -120,7 +120,7 @@ app.delete('/deletedata/:id', function(req,res) {
     console.log('\x1b[31m%s\x1b[0m','here');
     console.log('\x1b[31m%s\x1b[0m',req.param('id'));
     db.collection('cash').deleteOne({_id: new ObjectID(req.param('id'))},function(err, result) {
-        if (err) throw err;
+        if (err) {console.log(err)}
         else
         res.send('deleted');
     
