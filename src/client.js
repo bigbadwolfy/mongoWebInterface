7f8000f942577b343c746768407012e74c0674d4
Артемьев Игорь Борисович <IBArtemyev@alfabank.ru>
2018-05-28 14:21:13 +0300

    resize input text

--- src/client.js
+++ src/client.js
@@ -6,20 +6,15 @@ var selectJSON = 'selectFiles';
 
 
 function setText(textareaid, text) {
-
     document.getElementById(textareaid).innerHTML = text;
-
 }
 
 
 function loadJSON(){
-
     var files = document.getElementById(selectJSON).files;
     var value = document.getElementById(selectJSON).value;
-
-    var fr = new FileReader();
-
-//проверка на попытку загрузки пустого файла\
+    var filereader = new FileReader();
+//проверка на попытку загрузки пустого файла
     if (value == "") {
         
         console.log(document.getElementById(outtext).value);
@@ -29,14 +24,11 @@ function loadJSON(){
         else {
             var xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-
-                console.log(this.responseText);
-                alert('данные записаны:'+ this.responseText);
-
-            }
-        };
-
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                    alert('данные записаны:'+ this.responseText);
+                }
+            };
             xmlhttp.open('POST', document.URL+'senddata');
             xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
             xmlhttp.send(document.getElementById(outtext).value);
@@ -44,30 +36,25 @@ function loadJSON(){
     }
 
     else {
-    
-    fr.onload = function(e) {
 
-        var result = JSON.parse(e.target.result);
-        var formatted = JSON.stringify(result, null, 2);
-        document.getElementById(outtext).value = formatted;
-        var xmlhttp = new XMLHttpRequest();// new HttpRequest instance
+        filereader.onload = function(e) {
+            var result = JSON.parse(e.target.result);
+            var formatted = JSON.stringify(result, null, 2);
+            document.getElementById(outtext).value = formatted;
+            var xmlhttp = new XMLHttpRequest();// new HttpRequest instance
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
 
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-
-                setText(outtext,this.responseText);
-                console.log(this.responseText);
-                alert('данные записаны:'+this.responseText);
-
-            }
-        };
-
-        xmlhttp.open('POST', document.URL+'senddata');
-        xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        xmlhttp.send(formatted);
+                    setText(outtext,this.responseText);
+                    console.log(this.responseText);
+                    alert('данные записаны:'+this.responseText);
+                }
+            };
+            xmlhttp.open('POST', document.URL+'senddata');
+            xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            xmlhttp.send(formatted);
     };
-
-    fr.readAsText(files.item(0));
+        filereader.readAsText(files.item(0));
 }
 }
 
@@ -77,7 +64,7 @@ function findJSON() {
     var xmlhttp = new XMLHttpRequest();
     var key1 = document.getElementById('key').value;
     var val = document.getElementById('value').value;
-    var data = '{"'+key1+'":'+val+'}';
+    var data = '{'+'"'+key1+'"'+':'+val+'}';
     if ((val == '' && key1 != '') || (val != '' && key1 == '')) {
         alert('Please, enter key and value for search')
     }
@@ -89,7 +76,6 @@ function findJSON() {
                 console.log(this.responseText);
             }
         };
-
         xmlhttp.open('POST', document.URL + 'getdata');
         xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         if (data == '{"":}') {
@@ -109,7 +95,7 @@ function findlastJSON() {
         alert('Please, enter key and value for search')
     }
     else {
-    var data = '{"'+key1+'":'+val+'}';
+    var data = '{'+'"'+key1+'"'+':'+val+'}';
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById(intext).innerHTML =
@@ -117,7 +103,6 @@ function findlastJSON() {
             console.log(this.responseText);
         }
     };
-
     xmlhttp.open('POST', document.URL+'getlastdata');
     xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     if (data == '{"":}') {
